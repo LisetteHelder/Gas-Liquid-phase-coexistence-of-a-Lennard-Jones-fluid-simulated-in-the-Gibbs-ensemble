771b1ce8d439bf4f6f573f11173348a5c2356abc
prtnick <prettonicola02@gmail.com>
2026-05-10 20:32:57 +0200

    Update Gibbs_MC.c
    Change volume function added
    also a subroutine proposed_volume_move necessary for change_volume has been added, and a new  global structure type Volume_Move, used only for volume moves

--- Gibbs_MC.c
+++ Gibbs_MC.c
@@ -32,9 +32,21 @@ typedef struct {
 Box gas = { .n = 0, .energy=0, .input_file = "gas.dat",    .label = "gas" };
 Box liq = { .n = 0, .energy=0, .input_file = "liquid.dat", .label = "liq" };
 
+typedef struct {
+   double V_i; double V_f; 
+   double boxn;
+   double E_i; double E_f; 
+   double scale_factor; 
+   double r[N][NDIM];
+} Volume_Move; 
+
+Volume_Move Volume_Move_g; 
+Volume_Move Volume_Move_l; 
+
 
 double E_tot=0 ;
 double n_tot=0; 
+double V_tot;
 double e_cut;
 double dV = 0.0;
 
@@ -177,12 +189,14 @@ double distance_squared_pbc(const Box* b, const double a[NDIM], const double c[N
     return r2;
 }
 
-double particle_energy_at_position(const Box* b, const double pos[NDIM], int skip_index)
-{
-    double en = 0.0;
-    double r_cut2 = r_cut * r_cut;
-
-    for(int j = 0; j < b->n; ++j){
+double particle_energy_at_position(const Box* b, const double pos[NDIM], int skip_index) 
+{   // pos is just a point, we need this for particle_transfer,  
+    // because there will be a new proposed position that is of course not a position of a particle in either of the 2 boxes
+    // in that case it will be the skip_index value to be useless and it can be set to b->n   
+    // of course I'll want the new particle to interact with all the other particles in the box
+    double en = 0.0;                             
+    double r_cut2 = r_cut * r_cut;                                                   
+    for(int j = 0; j < b->n; ++j){                
         if(j == skip_index){
             continue;
         }
@@ -205,7 +219,7 @@ double particle_energy(const Box* b, int i)
 {
     assert(i >= 0 && i < b->n);
 
-    return particle_energy_at_position(b, b->r[i], i);
+     return particle_energy_at_position(b, b->r[i], i);
 }
 
 double box_energy(const Box* b)
@@ -236,15 +250,78 @@ int displacement(void)
     return 0;
 }
 
+void proposed_volume_move(Volume_Move*m, const Box*b){   // the const notation is just to signal that I am not modifying the old box (of course, it is the whole point of the function) I am just reading from it
+    Box trial=*b; // I make a box that is identical to the original one, but now I modify it 
+    for(int d=0; d<NDIM; d++){
+         trial.box[d]=m->boxn;
+    }
+    for(int n=0; n<trial.n; n++){
+        for(int d=0; d<NDIM; d++){
+            trial.r[n][d]=b->r[n][d]*(m->scale_factor); 
+        }
+    }  // here I have created a modified trial-box on which to test the new energy 
+
+    for(int n=0; n<trial.n; n++){
+        for(int d=0; d<NDIM; d++){
+            m->r[n][d]=trial.r[n][d]; 
+        } //these modified positions will be instead be put in the Volume_Move struct, they are conceptually different, this data is saved, and will update the true boxes if the move is accepted
+    }
+   m->E_f=0; 
+   for(int n=0; n<trial.n; n++){
+   m->E_f+= particle_energy_at_position(&trial,trial.r[n],n); 
+   }
+   m->E_f *=0.5; // don't count couple twice 
+   trial.energy=m->E_f; //just for the sake of clarity 
+}
+
 int change_volume(void)
-{
-    double tot_volume_i = box_volume(&gas) + box_volume(&liq);
-    double tot_volume_f = exp(log(tot_volume_i)+log(dV));
-    
-
-
-
+{   Volume_Move_g.V_i = box_volume(&gas); 
+    Volume_Move_l.V_i=V_tot-Volume_Move_g.V_i;
+    Volume_Move_g.V_f=Volume_Move_g.V_i+ dV*((dsfmt_genrand()-0.5)*2);
+    if(Volume_Move_g.V_f >=V_tot || Volume_Move_g.V_f<=0){
     return 0;
+    }
+    Volume_Move_l.V_f=V_tot-Volume_Move_g.V_f;  
+    if(pow(Volume_Move_g.V_f, 1.0 / NDIM) < 2.0 * r_cut){
+    return 0;
+    }
+    if(pow(Volume_Move_l.V_f, 1.0 / NDIM) < 2.0 * r_cut){
+        return 0;
+    }
+    Volume_Move_g.boxn=pow((Volume_Move_g.V_f),1.0/3.0);
+    Volume_Move_l.boxn=pow((Volume_Move_l.V_f),1.0/3.0);    
+    Volume_Move_g.scale_factor=pow((Volume_Move_g.V_f/Volume_Move_g.V_i),1.0/3.0);
+    Volume_Move_l.scale_factor=pow((Volume_Move_l.V_f/Volume_Move_l.V_i),1.0/3.0);
+    Volume_Move_g.E_i=gas.energy; 
+    Volume_Move_l.E_i=liq.energy; 
+    proposed_volume_move(&Volume_Move_g,&gas);
+    proposed_volume_move(&Volume_Move_l,&liq);
+    double deltaE_g=Volume_Move_g.E_f-Volume_Move_g.E_i;
+    double deltaE_l=Volume_Move_l.E_f-Volume_Move_l.E_i;
+    double deltaV_g=Volume_Move_g.V_f-Volume_Move_g.V_i; double deltaV_l=-deltaV_g;
+    double log_acc=-beta*deltaE_g-beta*deltaE_l+gas.n*log((Volume_Move_g.V_i+deltaV_g)/Volume_Move_g.V_i)+liq.n*log((Volume_Move_l.V_i+deltaV_l)/Volume_Move_l.V_i);
+    double u=dsfmt_genrand();
+    if (u <= 0.0) u = 1e-16; // in order not to have -inf (the consequence is I always refute moves that have probability 10^-16 which is not a problem)
+    if(log_acc>=0 || log(u)<log_acc){
+        gas.energy=Volume_Move_g.E_f;
+        liq.energy=Volume_Move_l.E_f;
+        for(int d=0; d<NDIM; d++){
+            gas.box[d]=Volume_Move_g.boxn; 
+            liq.box[d]=Volume_Move_l.boxn;
+        }
+        for(int n=0; n<gas.n; n++){
+            for(int d=0; d<NDIM; d++){
+                gas.r[n][d]=Volume_Move_g.r[n][d]; 
+            }
+        } 
+        for(int n=0; n<liq.n; n++){
+            for(int d=0; d<NDIM; d++){
+                liq.r[n][d]=Volume_Move_l.r[n][d];            }
+        } 
+        
+        return 1;
+    } else return 0;
+    
 }
 
 int particle_transfer(void)
@@ -269,10 +346,12 @@ int main(int argc, char* argv[])
     gas.energy = box_energy(&gas);
     liq.energy = box_energy(&liq);
     E_tot=gas.energy+liq.energy;
+    V_tot=box_volume(&gas)+box_volume(&liq);
+    
 
     printf("Starting gas volume:    %f\n", box_volume(&gas));
     printf("Starting liquid volume: %f\n", box_volume(&liq));
-    printf("Starting total volume:  %f\n", box_volume(&gas) + box_volume(&liq));
+    printf("Starting total volume:  %f\n", V_tot);
     printf("Starting total energy:        %f\n", E_tot);
     printf("Starting seed:          %lu\n", seed);
 
@@ -281,6 +360,7 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Error: could not write measurements.dat\n");
         exit(EXIT_FAILURE);
     }
+    //code goes here 
 
     fclose(fp);
 
