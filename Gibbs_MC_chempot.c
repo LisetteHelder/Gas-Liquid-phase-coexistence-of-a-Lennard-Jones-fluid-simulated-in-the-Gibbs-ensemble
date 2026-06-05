c4dc3933b23995a9c934e2ce82aba8fbf46a044b
LisetteHelder <l.helder@students.uu.nl>
2026-06-05 12:30:15 +0200

    Create Gibbs_MC_chempot.c
    chemical potential control plot generated (output file measurments_chempot.dat)

---
+++ Gibbs_MC_chempot.c
@@ -0,0 +1,569 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <math.h>
+#include "mt19937.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#define NDIM 3
+#define N 513
+
+const int    mc_steps        = 1e5;
+const int    output_steps    = 1e4;
+const double overall_density = 0.2; //
+const double delta           = 1.0;
+const double delta_V         = 0.01;//0.01;
+const double r_cut           = 2.5; 
+const double Temperature     = 0.7;  //
+const double beta            = 1.0 / Temperature;
+
+typedef struct {
+    int n;
+    double energy; 
+    double r[N][NDIM];
+    double box[NDIM];
+    const char* input_file;
+    const char* label;
+} Box;
+
+Box gas = { .n = 0, .energy=0, .input_file = "gas.dat",    .label = "gas" };
+Box liq = { .n = 0, .energy=0, .input_file = "gas.dat",    .label = "liq" };
+//Box liq = { .n = 0, .energy=0, .input_file = "liquid.dat", .label = "liq" };
+
+typedef struct {
+   double V_i; double V_f; 
+   double boxn;
+   double E_i; double E_f; 
+   double scale_factor; 
+   double r[N][NDIM];
+} Volume_Move; 
+
+Volume_Move Volume_Move_g; 
+Volume_Move Volume_Move_l; 
+
+
+double E_tot=0 ;
+double n_tot=0; 
+double V_tot;
+double e_cut;
+double dV = 0.0; // value will be assigned in the main
+
+/*FUNCTIONS*/
+
+double box_volume(const Box* b)
+{
+    double volume = 1.0;
+
+    for(int d = 0; d < NDIM; ++d){
+        volume *= b->box[d];
+    }
+
+    return volume;
+}
+
+void read_box(Box* b)
+{
+    FILE* fp = fopen(b->input_file, "r");
+    if(fp == NULL){
+        fprintf(stderr, "Error: could not open input file %s\n", b->input_file);
+        exit(EXIT_FAILURE);
+    }
+
+    if(fscanf(fp, "%d\n", &b->n) != 1){
+        fprintf(stderr, "Error: could not read number of particles from %s\n", b->input_file);
+        exit(EXIT_FAILURE);
+    }
+
+    if(b->n < 0 || b->n > N){
+        fprintf(stderr, "Error: file %s contains n = %d, but maximum allowed is N = %d and n must be > 0\n",
+                b->input_file, b->n, N);
+        exit(EXIT_FAILURE);
+    }
+
+    for(int d = 0; d < NDIM; ++d){
+        double dmin, dmax;
+
+        if(fscanf(fp, "%lf %lf\n", &dmin, &dmax) != 2){
+            fprintf(stderr, "Error: could not read box size from %s\n", b->input_file);
+            exit(EXIT_FAILURE);
+        }
+
+        b->box[d] = fabs(dmax - dmin);
+    }
+
+    for(int n = 0; n < b->n; ++n){
+        for(int d = 0; d < NDIM; ++d){
+            if(fscanf(fp, "%lf", &b->r[n][d]) != 1){
+                fprintf(stderr, "Error: could not read particle coordinates from %s\n", b->input_file);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        double diameter;
+
+        if(fscanf(fp, "%lf\n", &diameter) != 1){
+            fprintf(stderr, "Error: could not read particle diameter from %s\n", b->input_file);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(fp);
+}
+
+void read_data(void)
+{
+    read_box(&gas);
+    read_box(&liq);
+
+    n_tot = gas.n + liq.n;
+}
+
+void write_box(const Box* b, int step)
+{
+    char filename[128];
+    snprintf(filename, sizeof(filename), "coords_%s_step%07d.dat", b->label, step);
+
+    FILE* fp = fopen(filename, "w");
+    if(fp == NULL){
+        fprintf(stderr, "Error: could not write output file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(fp, "%d\n", b->n);
+
+    for(int d = 0; d < NDIM; ++d){
+        fprintf(fp, "%lf %lf\n", 0.0, b->box[d]);
+    }
+
+    for(int n = 0; n < b->n; ++n){
+        for(int d = 0; d < NDIM; ++d){
+            fprintf(fp, "%f\t", b->r[n][d]);
+        }
+
+        fprintf(fp, "%lf\n", 1.0);
+    }
+
+    fclose(fp);
+}
+
+void write_data(int step)
+{
+    write_box(&gas, step);
+    write_box(&liq, step);
+}
+
+void rescale_box(Box* b, double scale_factor)
+{
+    for(int n = 0; n < b->n; ++n){
+        for(int d = 0; d < NDIM; ++d){
+            b->r[n][d] *= scale_factor;
+        }
+    }
+
+    for(int d = 0; d < NDIM; ++d){
+        b->box[d] *= scale_factor;
+    }
+}
+
+void set_overall_density(void)
+{
+    double current_volume = box_volume(&gas) + box_volume(&liq);
+    double target_volume  = ((double)n_tot) / overall_density;
+    double scale_factor   = pow(target_volume / current_volume, 1.0 / NDIM);
+
+    rescale_box(&gas, scale_factor);
+    rescale_box(&liq, scale_factor);
+}
+
+double distance_squared_pbc(const Box* b, const double a[NDIM], const double c[NDIM])
+{
+    double r2 = 0.0;
+
+    for(int d = 0; d < NDIM; ++d){
+        double dr = a[d] - c[d];
+
+        dr -= b->box[d] * round(dr / b->box[d]); //pbc
+        r2 += dr * dr;
+    }
+
+    return r2;
+}
+
+double particle_energy_at_position(const Box* b, const double pos[NDIM], int skip_index) 
+{   // pos is just a point, we need this for particle_transfer,  
+    // because there will be a new proposed position that is of course not a position of a particle in either of the 2 boxes
+    // in that case it will be the skip_index value to be useless and it can be set to b->n   
+    // of course I'll want the new particle to interact with all the other particles in the box
+    double en = 0.0;                             
+    double r_cut2 = r_cut * r_cut;                                                   
+    for(int j = 0; j < b->n; ++j){                
+        if(j == skip_index){
+            continue;
+        }
+
+        double r2 = distance_squared_pbc(b, pos, b->r[j]);
+
+        if(r2 < 1e-12){
+            printf("Error: small distance in particle energy calculation. Exit function.\n");
+            exit;
+        }
+
+        if(r2 < r_cut2){
+            double inv_r2  = 1.0 / r2;
+            double inv_r6  = inv_r2 * inv_r2 * inv_r2;
+            double inv_r12 = inv_r6 * inv_r6;
+
+            en += 4.0 * (inv_r12 - inv_r6) - e_cut;
+        }
+    }
+
+    return en;
+}
+
+double particle_energy(const Box* b, int i)
+{
+     return particle_energy_at_position(b, b->r[i], i);
+}
+
+double box_energy(const Box* b)
+{
+    double en = 0.0;
+
+    for(int i = 0; i < b->n; ++i){
+        en += particle_energy(b, i);
+    }
+
+    return 0.5 * en;
+}
+
+void check_box(const Box* b)
+{
+    if(b->n == 0){
+        fprintf(stderr, "Error: number of particles in %s box is zero.\n", b->label);
+        exit(EXIT_FAILURE);
+    }
+
+    for(int d = 0; d < NDIM; ++d){
+        assert(r_cut <= 0.5 * b->box[d]);
+    }
+}
+
+double Widom_test(Box*b){
+    double U_plus = 0.0;
+    double mu_sum = 0.0;
+    double mu_av = 0.0;
+    int div = 0.0;
+    double r_added[NDIM];
+    double volume = b->box[0] * b->box[1] * b->box[2];
+
+    int ntests = 10000;
+    for(int i = 0; i <ntests; i++){
+        for(int d = 0; d < NDIM; d++){
+            r_added[d] = dsfmt_genrand() * (b->box[d]);
+        } 
+
+        U_plus = particle_energy_at_position(b, r_added, -1); //-1 so skip self in energy() function doesnt cause us to skip a particle
+        
+        if((- beta * U_plus) < 700){ //exp overflows at exp(706)
+            mu_sum += volume * exp(- beta * U_plus) / (b->n + 1);
+            div += 1;
+        }
+        else{
+            printf("- beta * U+ overflow. Value = %lf", - beta * U_plus);
+            continue;
+            //else mu_sum += volume * exp(- beta * - 100) / (b->n + 1);
+        }
+    }
+    mu_av = mu_sum / div;
+    if(mu_av == 0){
+        printf("Error: log(0) encountered in widom test");
+    }
+    mu_av = - Temperature * log(mu_av);
+    return mu_av;
+}
+
+/*MOVES*/
+//1. Displace particle within the same box
+
+int displacement(void)
+{
+// choose the box where to displace a random particle
+    Box* b = NULL;
+    double u=dsfmt_genrand(); 
+    
+    if(u < 0.5){
+        b = &gas;
+    } else if(u < 1.0){
+            b = &liq;
+        } else {
+                fprintf(stderr, "Error: invalid random number in displacement function: u = %f\n", u);
+                return -1;
+            }
+// choose the particle to be displaced 
+    int n=(int)((b->n)*dsfmt_genrand());
+    // save the old position
+    double old_pos[NDIM]; 
+    for(int d=0; d<NDIM; d++){
+        old_pos[d]=b->r[n][d]; 
+    }
+    double Old_particle_Energy=particle_energy(b,n);
+    //make a trial move 
+    for(int d=0; d<NDIM; d++){
+        double shift=(dsfmt_genrand() - 0.5) * 2.0 * delta;
+        b->r[n][d]+=shift; 
+        //respect PBC, keep particles in box
+        if(b->r[n][d]<0){b->r[n][d]+= b->box[d];}
+        if(b->r[n][d]>=b->box[d]){b->r[n][d]-= b->box[d];}
+    }
+    // test energy 
+    double Trial_particle_Energy=particle_energy(b,n); 
+    //Monte Carlo move 
+    double dE = Trial_particle_Energy - Old_particle_Energy;
+    if(dE < 0.0 || dsfmt_genrand() < exp(-beta * dE)){
+        //move accepted (also update energy of the box)
+        b->energy += dE; 
+        E_tot+=dE; //important because energy isn't conserved 
+        return 1;
+    }
+    else{ //move rejected (revert)
+        for(int d = 0; d < NDIM; ++d) b->r[n][d] = old_pos[d];
+        return 0; 
+    }
+}    
+
+//2. Volume change
+
+void proposed_volume_move(Volume_Move*m, const Box*b){   // the const notation is just to signal that I am not modifying the old box (of course, it is the whole point of the function) I am just reading from it
+    Box trial=*b; // I make a box that is identical to the original one, but now I modify it 
+    for(int d=0; d<NDIM; d++){
+         trial.box[d]=m->boxn;
+    }
+    for(int n=0; n<trial.n; n++){
+        for(int d=0; d<NDIM; d++){
+            trial.r[n][d]=b->r[n][d]*(m->scale_factor); 
+        }
+    }  // here I have created a modified trial-box on which to test the new energy 
+
+    for(int n=0; n<trial.n; n++){
+        for(int d=0; d<NDIM; d++){
+            m->r[n][d]=trial.r[n][d]; 
+        } //these modified positions will be instead be put in the Volume_Move struct, they are conceptually different, this data is saved, and will update the true boxes if the move is accepted
+    }
+   m->E_f=0; 
+   for(int n=0; n<trial.n; n++){
+   m->E_f+= particle_energy_at_position(&trial,trial.r[n],n); 
+   }
+   m->E_f *=0.5; // don't count couple twice 
+   trial.energy=m->E_f; //just for the sake of clarity 
+}
+
+int change_volume(void)
+{   Volume_Move_g.V_i = box_volume(&gas); 
+    Volume_Move_l.V_i=V_tot-Volume_Move_g.V_i;
+    Volume_Move_g.V_f=Volume_Move_g.V_i+ dV*((dsfmt_genrand()-0.5)*2);
+    if(Volume_Move_g.V_f >=V_tot || Volume_Move_g.V_f<=0){
+    return 0;
+    }
+    Volume_Move_l.V_f=V_tot-Volume_Move_g.V_f;  
+    if(pow(Volume_Move_g.V_f, 1.0 / NDIM) < 2.0 * r_cut){
+    return 0;
+    }
+    if(pow(Volume_Move_l.V_f, 1.0 / NDIM) < 2.0 * r_cut){
+        return 0;
+    }
+    Volume_Move_g.boxn=pow((Volume_Move_g.V_f),1.0/3.0);
+    Volume_Move_l.boxn=pow((Volume_Move_l.V_f),1.0/3.0);    
+    Volume_Move_g.scale_factor=pow((Volume_Move_g.V_f/Volume_Move_g.V_i),1.0/3.0);
+    Volume_Move_l.scale_factor=pow((Volume_Move_l.V_f/Volume_Move_l.V_i),1.0/3.0);
+    Volume_Move_g.E_i=gas.energy; 
+    Volume_Move_l.E_i=liq.energy; 
+    proposed_volume_move(&Volume_Move_g,&gas);
+    proposed_volume_move(&Volume_Move_l,&liq);
+    double deltaE_g=Volume_Move_g.E_f-Volume_Move_g.E_i;
+    double deltaE_l=Volume_Move_l.E_f-Volume_Move_l.E_i;
+    double deltaV_g=Volume_Move_g.V_f-Volume_Move_g.V_i; double deltaV_l=-deltaV_g;
+    double log_acc=-beta*deltaE_g-beta*deltaE_l+gas.n*log((Volume_Move_g.V_i+deltaV_g)/Volume_Move_g.V_i)+liq.n*log((Volume_Move_l.V_i+deltaV_l)/Volume_Move_l.V_i);
+    double u=dsfmt_genrand();
+    if (u <= 0.0) u = 1e-16; // in order not to have -inf (the consequence is I always refute moves that have probability 10^-16 which is not a problem)
+    if(log_acc>=0 || log(u)<log_acc){
+        gas.energy=Volume_Move_g.E_f;
+        liq.energy=Volume_Move_l.E_f;
+        E_tot=gas.energy+liq.energy; 
+        for(int d=0; d<NDIM; d++){
+            gas.box[d]=Volume_Move_g.boxn; 
+            liq.box[d]=Volume_Move_l.boxn;
+        }
+        for(int n=0; n<gas.n; n++){
+            for(int d=0; d<NDIM; d++){
+                gas.r[n][d]=Volume_Move_g.r[n][d]; 
+            }
+        } 
+        for(int n=0; n<liq.n; n++){
+            for(int d=0; d<NDIM; d++){
+                liq.r[n][d]=Volume_Move_l.r[n][d];            }
+        } 
+        
+        return 1;
+    } else return 0;
+    
+}
+//3. Particle Transfer
+
+int particle_transfer(void){
+    Box*target = NULL;
+    Box*source = NULL;
+    double u = dsfmt_genrand();
+
+    //choose source and target randomly (50/50)
+    if(u < 0.5){
+        target = &gas;
+        source = &liq;
+    } else {
+        target = &liq;
+        source = &gas;
+    }
+
+    //energy of removing a random particle:
+    int s = (int)(dsfmt_genrand() * source->n); //particle removed from source (if accepted)
+    double E_removed_particle = particle_energy_at_position(source, source->r[s], s); 
+
+    //energy of adding a particle at random position:
+    int t = (int)(dsfmt_genrand() * target->n); //particle added to target (if accepted)
+    double r_added[NDIM];
+    for(int d = 0; d < NDIM; d++){
+        double side = target->box[d];
+        double random_coordinate = dsfmt_genrand() * side; //i think theres no need for pbc now
+        r_added[d]=random_coordinate;
+    }
+    double E_added_particle = particle_energy_at_position(target, r_added, t); //prob: this becomes huge e7 probably due to overlap since i inserted a fcc file hahha
+
+    //calculate energy difference
+    double deltaE = E_added_particle - E_removed_particle; 
+    double V_s = pow(source->box[0],3); //volume source
+    double V_t = pow(target->box[0],3); //volume target 
+    double configE = ( ( (target->n)+1 ) * V_s)  / ((source->n)* V_t);
+
+    if(deltaE < 0.0 || dsfmt_genrand() < exp(-beta * (deltaE) - log(configE))){
+        //accept: update energies
+        target->energy += E_added_particle; 
+        source->energy -= E_removed_particle;
+        //update particle nr
+        target->n += 1;
+        source->n -= 1;
+        //update positions
+        for(int d=0; d<NDIM; d++){
+            target->r[target->n][d]=r_added[d]; // the particle is added on the n+1 th index for simplicity
+        }
+        return 1;
+    } 
+    else {
+        return 0;
+    }
+}
+
+
+int main(int argc, char* argv[]){
+    printf("test 420\n");
+    assert(delta > 0.0);
+    e_cut = 4.0 * (pow(1.0 / r_cut, 12.0) - pow(1.0 / r_cut, 6.0)); //did you not make this a simulation variable on purpose?
+
+    read_data();
+    set_overall_density();  // Rescale to correct density so particles interact from start
+    printf("nr of particles %lf\n", n_tot);
+    check_box(&gas);
+    check_box(&liq);
+    printf("\n ngas=%d , nliq=%d\n", gas.n ,liq.n);
+    
+    size_t seed = time(NULL);
+    dsfmt_seed(seed);
+    printf("box volume: gas = %lf, liq= %lf \n", box_volume(&gas), box_volume(&liq));
+
+    dV = delta_V * (box_volume(&gas) + box_volume(&liq)); 
+    gas.energy = box_energy(&gas); 
+    liq.energy = box_energy(&liq);
+    E_tot=gas.energy+liq.energy;
+    V_tot=box_volume(&gas)+box_volume(&liq);
+
+    printf("Starting gas volume:    %f\n", box_volume(&gas));
+    printf("Starting liquid volume: %f\n", box_volume(&liq));
+    printf("Starting total volume:  %f\n", V_tot);
+    printf("Starting total energy:  %lf\n", E_tot);
+    printf("Starting seed:          %lu\n", seed);
+
+    FILE* fp2 = fopen("measurements_chempot.dat", "w");
+    if(fp2 == NULL){
+        fprintf(stderr, "Error: could not write measurements.dat\n");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    //ratios
+    double ratio_displacement= 10;
+    double ratio_volumechange = 100; //1
+    double ratio_transfer = 100; //1000
+    double total_ratio = ratio_displacement + ratio_transfer + ratio_volumechange;
+    double displacment_divide = ratio_displacement; 
+    double changevolume_divide = ratio_displacement + ratio_volumechange; 
+    double transfer_divide = total_ratio;
+
+    //fprintf(fp1, "t \t E_tot \t\t n_gas \t n_liq \t V_gas \t\t V_liq \t rho_gas \t\t rho_liq\n"); 
+    //fprintf(fp2, "step \t\t E_tot \t\t n_gas \t n_liq \t\t V_gas \t\t\t V_liq \t\t\t rho_gas \t\t\t rho_liq \t\t\t mu_gas \t\t\t mu_liq\n"); 
+    fprintf(fp2, "step, density_gas, mu_gas, density_liquid, mu_liq\n");
+
+    int accepted = 0; //maybe keep track seperately of each move
+    int accepted_v = 0; int tried_v = 0;
+    int accepted_d = 0;
+    int accepted_tr = 0;
+    /* chemical potential running sums and sample counter */
+    double mu_gas_sum = 0.0;
+    double mu_liq_sum = 0.0;
+    double mu_gas_av = 0.0;
+    double mu_liq_av = 0.0;
+    int mu_count = 0;
+    for(int step=0; step < mc_steps; step++){
+        
+        //select a random step
+        double  mc_move_extraction = dsfmt_genrand()*total_ratio;
+        if(mc_move_extraction<0){perror("invalid value of mc_move_extraction \n");}
+
+        if(mc_move_extraction < displacment_divide){
+            accepted_d += displacement(); 
+        }   else if(mc_move_extraction < changevolume_divide){
+            tried_v += 1;
+            accepted_v += change_volume();  // printf("volume move attempted.  Performed (yes 1/ no 0) = %d \n", out_ch_v);
+            }   else if(mc_move_extraction <= transfer_divide){
+                    accepted_tr += particle_transfer(); 
+                } else perror("there is some mistake in the implementation of the mc step, dsfmt_genrand can't generate numbers greater than 1 \n");
+        
+        double volume_gas = gas.box[0]*gas.box[1]*gas.box[2];
+        double volume_liquid = liq.box[0]*liq.box[1]*liq.box[2];
+        double density_gas = gas.n / volume_gas;
+        double density_liquid = liq.n / volume_liquid;
+
+        //chemical potential equation 9
+        if(step % 100 == 0){
+            double mu_gas = Widom_test(&gas);
+            double mu_liq = Widom_test(&liq);
+
+            //fprintf(fp1, "%d \t %lf \t %d \t %d \t %lf \t %lf \t %lf \t %lf \t %lf \t %lf \n", step, E_tot, gas.n, liq.n, volume_gas, volume_liquid, density_gas, density_liquid, mu_gas, mu_liq); 
+            fprintf(fp2, "%d \t %lf \t %lf \t %lf \t %lf\n", step, density_gas, mu_gas, density_liquid, mu_liq);
+        }
+
+        if(step % output_steps == 0){
+            printf("Step %d. Move acceptances: volume %lf (%d tries) \t displ. %lf \t transfer %lf. \n", step, (double)accepted_v/output_steps, tried_v, (double)accepted_d/output_steps, (double)accepted_tr/output_steps);
+            printf("n_gas = %d \t n_liq = %d \n", gas.n, liq.n);
+            accepted = 0;
+            accepted_v = 0;
+            accepted_d = 0;
+            accepted_tr = 0;
+            //write_data(step);
+        }
+    }
+    write_data(mc_steps);
+    fclose(fp2);
+    return 0;
+}
